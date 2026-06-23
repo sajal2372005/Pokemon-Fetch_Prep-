bb4822138500d3c8053c174fb19b84bcadd901ba
Sajal Goswami <sajal2372005@gmail.com>
2026-06-23 18:38:12 +0530

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,58 @@
 import React,{useState,useEffect} from 'react';
 import Cards from './Components/Cards.jsx'
 import "./Css/App.css"
+import {FaArrowRight, FaArrowLeft}from 'react-icons/fa'
 
 
 
 
 
 const App = () =>{
+  const [search,setSearch] = useState("")
+  const [allPokemon,setAllPokemon] = useState([])
   const [pokemon,setPokemon] = useState([])
   const [count,setCount] = useState(0)
   useEffect(()=>{
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=100`)
+    .then((res)=>res.json())
+    .then((res)=>setAllPokemon(res.results))
+  },[])
+  useEffect(()=>{
+
     fetch(`https://pokeapi.co/api/v2/pokemon?offset=${count}&limit=8`)
     .then((res)=>res.json())
     .then((res)=>setPokemon(res.results))
   },[count])
+
+  useEffect(()=>{
+    search.length === 0 ? fetch(`https://pokeapi.co/api/v2/pokemon?offset=${count}&limit=8`)
+    .then((res)=>res.json())
+    .then((res)=>setPokemon(res.results)) 
+    
+    :
+
+    
+    setPokemon(allPokemon.filter((item)=> item.name.toLowerCase().includes(search.toLowerCase())))
+},[search])
+  
   return(
     <>
-    <button onClick={()=>setCount(count-10)} style={{position:"absolute",top:"5%",left:"5%",transform:"translateY(-50%)",backgroundColor:"rgb(255, 255, 255)",borderRadius:"5px",padding:"10px",fontSize:"20px",fontWeight:"bold"}}>Prev</button>
-    <button onClick={()=>setCount(count+10)} style={{position:"absolute",top:"5%",right:"5%",transform:"translateY(-50%)",backgroundColor:"rgb(255, 255, 255)",borderRadius:"5px",padding:"10px",fontSize:"20px",fontWeight:"bold"}}>Next</button>
-      
+    <button onClick={()=>setCount(count-1)} style={{position:"absolute",top:"5%",left:"5%",transform:"translateY(-50%)",backgroundColor:"rgb(255, 255, 255)",borderRadius:"5px",padding:"10px",fontSize:"20px",fontWeight:"bold"}}><FaArrowLeft/></button>
+    <button onClick={()=>setCount(count+1)} style={{position:"absolute",top:"5%",right:"5%",transform:"translateY(-50%)",backgroundColor:"rgb(255, 255, 255)",borderRadius:"5px",padding:"10px",fontSize:"20px",fontWeight:"bold"}}><FaArrowRight/></button>
+    <div>
+      <input type="text" placeholder='Search Pokemon' value = {search} onChange={(e)=>setSearch(e.target.value)} style={{position:"absolute",top:"5%",left:"50%",transform:"translateX(-50%)",backgroundColor:"rgb(255, 255, 255)",borderRadius:"5px",padding:"10px",fontSize:"20px",fontWeight:"bold"}}/>
+    </div>
+     
     <div id='homeMainDiv' style={{paddingTop:"10vh"}}>
-      {
+      <div id="cardsDiv">
+        {
         pokemon.map((item,index)=>{
           return(
             <Cards key={index} name = {item.name} url = {item.url}/>
           )
         })
       }
+      </div>
       
     </div>
     </>
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -13,10 +13,54 @@ const Cards = ({key,name,url}) =>{
         
         <>
         
-        <div style = {{height:"30vh",width:"20%",backgroundColor:"rgb(255, 255, 255)",borderRadius:"1px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-            {state && <img id = "pokemon-image"  onMouseEnter={()=>setImage(true)} onMouseLeave={()=>setImage(false)} src={image ? state.sprites?.other?.showdown?.front_default : state.sprites?.other?.dream_world?.front_default}  alt={name} style={{width:"100px",height:"100px",objectFit:"contain"}}/>}
-            <h1 style={{fontSize:"26px",fontWeight:"bold",fontFamily:"revert-layer"}}>{name}</h1>
-            <p style={{fontSize:"15px",fontFamily:"Arial, sans-serif"}}>weight: {state?.weight} kg</p>
+        <div style = {{height:"34vh",width:"24%",backgroundColor:"rgb(255, 255, 255)",borderRadius:"1px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+            {state && <img id = "pokemon-image"  onMouseEnter={()=>setImage(true)} onMouseLeave={()=>setImage(false)} src={image ? state.sprites?.other?.showdown?.front_default : state.sprites.other.dream_world.front_default !== null? state.sprites.other.dream_world.front_default : state.sprites.other.home.front_default !== null ? state.sprites.other.home.front_default:state.sprites.front_default}  alt={name} style={{width:"100px",height:"100px",objectFit:"contain"}}/>}
+            <h1 style={{fontSize:"24px",fontWeight:"bold",fontFamily:"Arial, sans-serif"}}>{name}</h1>
+            <button style={{backgroundColor:"lightgreen",borderRadius:"5px",fontSize:"18px",paddingLeft:"10px",paddingRight:"10px"}}>{state?.types.map((type)=>type.type.name).join(", ")}</button>
+            <div style={{display:"flex",justifyContent:"space-evenly",width:"100%",marginTop:"10px"}}>
+                <div style={{display:"flex"}}>
+                <p style={{fontWeight:"bold"}}>
+                    Height :
+                </p>
+                 {state?.height}
+                </div>
+                <div style={{display:"flex"}}>
+                <p style={{fontWeight:"bold"}}>
+                    Weight :
+                </p>
+                 {state?.weight}
+                </div>
+                <div style={{display:"flex"}}>
+                <p style={{fontWeight:"bold"}}>
+                    Speed :
+                </p>
+                 {state?.stats[5].base_stat}
+                </div>
+                
+            </div>
+
+
+
+            <div style={{display:"flex",justifyContent:"space-evenly",width:"100%",marginTop:"4px"}}>
+                <div style={{display:"flex"}}>
+                <p style={{fontWeight:"bold"}}>
+                    Experience :
+                </p>
+                 {state?.base_experience}
+                </div>
+                <div style={{display:"flex"}}>
+                <p style={{fontWeight:"bold"}}>
+                    attack :
+                </p>
+                 {state?.stats[1].base_stat}
+                </div>
+                
+                
+            </div>
+
+
+
+
         </div>
         </>
     )
